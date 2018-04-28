0d0589457adf6cc7cc9c1a891f4fb2518448a2d8
José Aleixo <josealeixo.pc@gmail.com>
2018-04-28 13:47:03 +0100

    Added GPU scrapper.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const createFile = function (filePath, content) {
   })
 }
 
-const main = function () {
+const getCPUs = function () {
   axios.get('https://www.cpubenchmark.net/laptop.html').then(function (response) {
     if (response.status === 200) {
       var $ = cheerio.load(response.data)
@@ -30,6 +30,10 @@ const main = function () {
 
         currentCPU = $(elem).find('td:first-child a').text()
 
+        if (currentCPU.length === 0) {
+          return true // if the CPU has no name, skip it
+        }
+
         rankingString += currentRank + ',' + currentCPU + '\n'
 
         currentRank++
@@ -42,6 +46,43 @@ const main = function () {
   }).catch(function (error) {
     console.error(error)
   })
+}
+
+const getGPUs = function () {
+  axios.get('https://www.notebookcheck.net/Mobile-Graphics-Cards-Benchmark-List.844.0.html?type=&sort=&showClassDescription=1&archive=1&perfrating=1&or=0&gpu_fullname=1').then(function (response) {
+    if (response.status === 200) {
+      var $ = cheerio.load(response.data)
+
+      var rankingString = ''
+      var currentRank = 1
+
+      // Skip first child, because it is the header of the table
+      $('#sortierbare_tabelle tr.odd').each(function (i, elem) {
+        var currentGPU
+
+        currentGPU = $(elem).find('td a').text()
+
+        if (currentGPU.length === 0) {
+          return true // if the GPU has no name, skip it
+        }
+
+        rankingString += currentRank + ',' + currentGPU + '\n'
+
+        currentRank++
+      })
+
+      createFile('gpu.csv', rankingString)
+    } else {
+      console.error('Bad response status.\n' + response.status)
+    }
+  }).catch(function (error) {
+    console.error(error)
+  })
+}
+
+const main = function () {
+  getCPUs()
+  getGPUs()
 }
 
 main()
